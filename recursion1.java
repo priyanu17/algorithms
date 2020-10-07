9ad5d1f6af453821d5e9af1e39f4b45142155ce6
Anu Priya <53366411+priyanu17@users.noreply.github.com>
2020-10-07 12:56:38 +0530

    cm

---
+++ recursion1.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+class recursion1{
+
+    public static int sumOfNumbers(int num){
+
+        //base condition
+        if (num == 1){
+            return 1;
+        }
+
+        return num + sumOfNumbers(num - 1);
+
+    }
+
+    public static void main(String ar [] ){
+
+        System.out.println("\nSum of first n natural numbers\n");
+
+        System.out.println("Enter natural number to find sum upto");
+        Scanner scan = new Scanner(System.in);
+        int numberToSum = scan.nextInt();
+        int result = sumOfNumbers(numberToSum);
+        System.out.println("Sum of first " + numberToSum + " natural numbers is : " + result);
+
+        scan.close();
+    }
+}//end of class
