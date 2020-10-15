180aefe076620fe6eef42b2f44baa79e6f7ec113
Anu Priya <53366411+priyanu17@users.noreply.github.com>
2020-10-15 15:19:45 +0530

    cm

--- Sorting/insertionSort.java
+++ Sorting/insertionSort.java
@@ -1,25 +1,26 @@
 package Sorting;
 
-public class insertionSort{
+public class insertionSort {
     public static void main(String args [] ){
 
-        int arr [] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+        
+        int arr [] = {99, 88, 77, 66, 55, 44, 33, 22, 11};
 
         System.out.println("Array before sorting");
         for(int element : arr) System.out.print(element + "  ");
         System.out.println();
 
-        //applying insertion sort
-        for(int i=0; i<arr.length; i++){
-            int valToInsert = arr[i];
-            int j= i-1;
-
-            while(j>=0 && arr[j] > valToInsert){
+        //insertion sort
+        for(int i=1; i<arr.length; i++){
+            int valToInsert= arr[i];
+            int j=i-1;
+            while(j >=0 && arr[j] > valToInsert){
                 arr[j+1] = arr[j];
                 j--;
             }
+
             arr[j+1] = valToInsert;
-        }
+        }   
 
         System.out.println("Array after sorting");
         for(int element : arr) System.out.print(element + "  ");
--- Sorting/selectionSort.java
+++ Sorting/selectionSort.java
@@ -1,30 +1,31 @@
 package Sorting;
 
-public class selectionSort {
-    
-    public static void main(String args [] ) {
-
-        //selection sort 
-        int arr [] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+public class selectionSort{
+    public static void main(String args []){
+        
+        int arr [] = {99, 88, 77, 66, 55, 44, 33, 22, 11};
 
         System.out.println("Array before sorting");
         for(int element : arr) System.out.print(element + "  ");
         System.out.println();
 
-        //applying selection sort
-        for( int i= 0; i<arr.length-1; i++){
+        //selection sort
+        for(int i=0; i<arr.length-1; i++){
             int minIndex = i;
 
             for(int j=i+1; j<arr.length; j++)
-                if(arr[j] < arr[minIndex]) minIndex = j;
+                if( arr[j] < arr[minIndex]) minIndex = j;
 
-                int temp = arr[i];
-                arr[i] = arr[minIndex];
-                arr[minIndex] = temp;
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+
         }
         System.out.println("Array after sorting");
         for(int element : arr) System.out.print(element + "  ");
         System.out.println();
-
     }
+
+
+
 }
